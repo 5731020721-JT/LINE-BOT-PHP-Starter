4bd0005645a84d8220901a6ed328ddad9291bb5a
Jettapat <hennet2@hotmail.com>
2018-02-07 16:07:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,47 @@
-curl -X POST \
--H 'Content-Type:application/json' \
--H 'Authorization: Bearer ip73DuNTTqtvZZI3PBZZ5Bv37EnHFluMKnQu91J0YuHi4ftF2YYorQnLhqnhNyQqRRJXvIxZ3dv8VCdMPf+N86gl06g5M7gzty9a/pvf/2IHgbQWUDvyajes+/9L3WtrH3MpIA5xhWx8SlcbG/nsqAdB04t89/1O/w1cDnyilFU=' \
-...
+<?php
+$access_token = 'ip73DuNTTqtvZZI3PBZZ5Bv37EnHFluMKnQu91J0YuHi4ftF2YYorQnLhqnhNyQqRRJXvIxZ3dv8VCdMPf+N86gl06g5M7gzty9a/pvf/2IHgbQWUDvyajes+/9L3WtrH3MpIA5xhWx8SlcbG/nsqAdB04t89/1O/w1cDnyilFU=';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";

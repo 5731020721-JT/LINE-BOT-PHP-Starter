cca0a26207eba65eae8729177888ffbc9a5c4c74
Jettapat <hennet2@hotmail.com>
2018-02-07 15:56:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,2 +1,4 @@
-<?php
-echo "I am a bot";
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer ip73DuNTTqtvZZI3PBZZ5Bv37EnHFluMKnQu91J0YuHi4ftF2YYorQnLhqnhNyQqRRJXvIxZ3dv8VCdMPf+N86gl06g5M7gzty9a/pvf/2IHgbQWUDvyajes+/9L3WtrH3MpIA5xhWx8SlcbG/nsqAdB04t89/1O/w1cDnyilFU=' \
+...
